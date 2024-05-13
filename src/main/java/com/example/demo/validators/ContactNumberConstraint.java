7c62eedffbf42f3564078a5bb277421cc8ea382f
ANA <anaogrizovic11@gmail.com>
2024-05-13 11:42:34 +0200

    Ana Ogrizovic 2021201709 SII

---
+++ src/main/java/com/example/demo/validators/ContactNumberConstraint.java
@@ -0,0 +1,17 @@
+package com.example.demo.validators;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ContactNumberValidators.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface ContactNumberConstraint {
+    String message() default "Invalid phone number";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
